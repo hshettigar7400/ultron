421ce6c9988e69d5a59cd694234092b0b2f9e836
Bharat <Bharat>
2017-05-09 18:52:09 +0530

    adding code for portrait

--- app/components/template/StaticPage1.jsx
+++ app/components/template/StaticPage1.jsx
@@ -37,6 +37,34 @@ export default class StaticPage1 extends React.Component {
               </div>
             </div>
         </MediaQuery>
+        <MediaQuery query='(orientation: portrait)'>
+          <div className="content">
+            <div className="bg">
+              <div className="intro white">
+                <div className="flex-container">
+                  <div className="left-side">
+                    <div className="intro_heading">
+                      <h1>Introduction to SEC </h1>
+                    </div>
+                    <div className="intro_para">
+                      <p>As an investment adviser, you need to be aware of all the rules and laws pertaining to the United States securities regulation. </p>
+                      <span>Securities and Exchange Commission (Sec):</span>
+                    </div>
+                    <div className="intro_list">
+                      <ul>
+                        <li>Makes the rules</li>
+                        <li>Enforces policy</li>
+                        <li>Oversees the markets</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="right-side">
+                    <img src="/app/assets/images/template/image1.jpg" /></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </MediaQuery>
       </div>
         )
       }
