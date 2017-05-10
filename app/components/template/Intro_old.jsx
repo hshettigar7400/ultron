f236f48fd3dd982d9fc57cc71cc459135c8b97a3
Bharat <Bharat>
2017-05-10 20:24:51 +0530

    adding css for phone

--- app/components/template/Intro_old.jsx
+++ app/components/template/Intro_old.jsx
@@ -61,9 +61,7 @@ export default class Intro extends React.Component {
               </div>
             </div>
           </MediaQuery>
-
-
-                    <MediaQuery query='(min-device-width: 768px) and (max-device-width: 1024px)'>
+          <MediaQuery query='(min-device-width: 768px) and (max-device-width: 1024px)'>
             <div className="content">
               <div className="bg">
                 <div className="logo"> <img src="/app/assets/images/template/logo.jpg"/></div>
@@ -85,22 +83,20 @@ export default class Intro extends React.Component {
               </div>
             </div>
           </MediaQuery>
-
-
           <MediaQuery query='(min-device-width: 320px) and (max-device-width: 736px)'>
             <div className="content">
               <div className="bg">
                 <div className="logo"> <img src="/app/assets/images/template/logo.jpg"/></div>
 
                 <div className="title welcome_text">
-                  <h1 className="white">Welcome to the course,</h1>
+                  <h1 className="white">portrait,</h1>
                   <h1 className="black">SEC Compliance Readiness</h1>
                   <div className="flex-container white">
                     <div className="left-side ">Standards <br/>Rules <br/>Regulations</div>
                     <div className="right-side ">Strategy <br/>Governance <br/>Security</div>
                   </div>
                 </div>
-
+                <br/>
                 <div className="btn white">
                   <span className="instruction_text">Click Start to begin </span>
                   <a href="#" className="start-button" role="button" onClick={this.startCourse.bind(this)}>
@@ -110,7 +106,6 @@ export default class Intro extends React.Component {
               </div>
             </div>
           </MediaQuery>
-
         </div>
       )
     }
