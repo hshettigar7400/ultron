0a962d1be766e17fdc4b67b2e285e7e61a4a16b6
Bharat <Bharat>
2017-05-12 02:44:11 +0530

    adding page3 and page5

--- app/components/template/StaticPage3.jsx
+++ app/components/template/StaticPage3.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 
 export default class StaticPage3 extends React.Component {
   constructor(props) {
@@ -9,20 +10,52 @@ export default class StaticPage3 extends React.Component {
     return (
       <div className="static-page3">
         <div className="content">
-          <div className="bg">
-            <div className="intro white">
-              <div className="flex-container">
-                <div className="left-side">
-                  <div className="intro_heading">
-                    <h1>Our Goal</h1>
-                  </div>
-                  <div className="intro_para">
-                    <span>Our goal is to help you be better prepared by knowing what to expect, which will make a positive impression with the SEC, and keep you focused on your work.</span>
+        <div className="bg">
+         <MediaQuery query='(min-device-width: 1224px)'>
+
+                <div className="intro white">
+                  <div className="flex-container">
+                    <div className="left-side">
+                      <div className="intro_heading">
+                        <h1>Our Goal</h1>
+                      </div>
+                      <div className="intro_para">
+                        <span>Our goal is to help you be better prepared by knowing what to expect, which will make a positive impression with the SEC, and keep you focused on your work.</span>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
+          </MediaQuery>
+          <MediaQuery query='(min-device-width: 768px) and (max-device-width: 1024px)'>
+                <div className="intro white">
+                  <div className="flex-container">
+                    <div className="left-side">
+                      <div className="intro_heading">
+                        <h1>Our Goal</h1>
+                      </div>
+                      <div className="intro_para">
+                        <span>Our goal is to help you be better prepared by knowing what to expect, which will make a positive impression with the SEC, and keep you focused on your work.</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+          </MediaQuery>
+          <MediaQuery query='(min-device-width: 320px) and (max-device-width: 736px)'>
+                <div className="intro white">
+                  <div className="flex-container">
+                    <div className="left-side">
+                      <div className="intro_heading">
+                        <h1>Our Goal</h1>
+                      </div>
+                      <div className="intro_para">
+                        <span>Our goal is to help you be better prepared by knowing what to expect, which will make a positive impression with the SEC, and keep you focused on your work.</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+          </MediaQuery>
+        </div>
         </div>
       </div>
     )
