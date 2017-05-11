09187173b4ed7ef41bb9e40136e415f018cb492f
Bharat <Bharat>
2017-05-12 03:32:47 +0530

    adding pages

--- app/components/template/Interactivity2.jsx
+++ app/components/template/Interactivity2.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import MediaQuery from 'react-responsive';
 export default class Interactivity2 extends React.Component {
   constructor(props) {
     super(props);
@@ -52,6 +52,7 @@ export default class Interactivity2 extends React.Component {
   render() {
     return (
       <div className="interactivity2">
+        <MediaQuery query='(min-device-width: 1224px)'>
         <div className="content">
           <div className="bg">
             <div className="intro white">
@@ -64,19 +65,20 @@ export default class Interactivity2 extends React.Component {
                     <span>Who they apply to</span>
                   </div>
                   <div className="btn_div">
-                    <span className="instruction_text">
-                      <img src="/app/assets/images/template/hand_icon.png"/>
-                      <span>Click each tab to know more.</span>
+                    <span className="instruction_img">
+                      <img src="/app/assets/images/template/hand_icon.png" className="middle_align"/>
+                      <span className="instruction_text">Click each tab to know more.</span>
                     </span>
                   </div>
                   <div className="container">
                     <div className="buttons">
                       <span><button id="btn_01" onClick={this.openTabContainer.bind(this,1)}
                         className={this.state.currentTab === 1 ? "selected" : ""}>Traditional Examination</button></span>
-                      <span><button id="btn_02" onClick={this.openTabContainer.bind(this,2)}
+                     <span><button id="btn_02" onClick={this.openTabContainer.bind(this,2)}
                         className={this.state.currentTab === 2 ? "selected" : ""}>Dual registrant exams</button></span>
-                      <span><button id="btn_03" onClick={this.openTabContainer.bind(this,3)}
-                        className={this.state.currentTab === 3 ? "selected" : ""}>Presence Exam</button></span>
+                     <span><button id="btn_03" onClick={this.openTabContainer.bind(this,3)}
+                        className={this.state.currentTab === 3 ? "selected" : ""}>Presence Exam</button>
+                        </span>
                     </div>
                     {this.displayTab()}
                   </div>
@@ -85,6 +87,78 @@ export default class Interactivity2 extends React.Component {
               </div>
             </div>
           </div>
+        </MediaQuery>
+        <MediaQuery query='(min-device-width: 768px) and (max-device-width: 1024px)'>
+            <div className="content">
+          <div className="bg">
+            <div className="intro white">
+              <div className="flex-container">
+                <div className="left-side">
+                  <div className="intro_heading">
+                    <h1>Kinds of Examinations</h1>
+                  </div>
+                  <div className="intro_para">
+                    <span>Who they apply to</span>
+                  </div>
+                  <div className="btn_div">
+                    <span className="instruction_img">
+                      <img src="/app/assets/images/template/hand_icon.png" className="middle_align"/>
+                      <span className="instruction_text">Click each tab to know more.</span>
+                    </span>
+                  </div>
+                  <div className="container">
+                    <div className="buttons">
+                      <span><button id="btn_01" onClick={this.openTabContainer.bind(this,1)}
+                        className={this.state.currentTab === 1 ? "selected" : ""}>Traditional Examination</button></span>
+                     <span><button id="btn_02" onClick={this.openTabContainer.bind(this,2)}
+                        className={this.state.currentTab === 2 ? "selected" : ""}>Dual registrant exams</button></span>
+                    <span> <button id="btn_03" onClick={this.openTabContainer.bind(this,3)}
+                        className={this.state.currentTab === 3 ? "selected" : ""}>Presence Exam</button>
+                        </span>
+                    </div>
+                    {this.displayTab()}
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MediaQuery>
+        <MediaQuery query='(min-device-width: 320px) and (max-device-width: 736px)'>
+            <div className="content">
+          <div className="bg">
+            <div className="intro white">
+              <div className="flex-container">
+                <div className="left-side">
+                  <div className="intro_heading">
+                    <h1>Kinds of Examinations</h1>
+                  </div>
+                  <div className="intro_para">
+                    <span>Who they apply to</span>
+                  </div>
+                  <div className="btn_div">
+                    <span className="instruction_img">
+                      <img src="/app/assets/images/template/hand_icon.png" className="middle_align"/>
+                      <span className="instruction_text">Click each tab to know more.</span>
+                    </span>
+                  </div>
+                  <div className="container">
+                    <div className="buttons">
+                      <button id="btn_01" onClick={this.openTabContainer.bind(this,1)}
+                        className={this.state.currentTab === 1 ? "selected" : ""}>Traditional Examination</button>
+                     <button id="btn_02" onClick={this.openTabContainer.bind(this,2)}
+                        className={this.state.currentTab === 2 ? "selected" : ""}>Dual registrant exams</button>
+                     <button id="btn_03" onClick={this.openTabContainer.bind(this,3)}
+                        className={this.state.currentTab === 3 ? "selected" : ""}>Presence Exam</button>
+                    </div>
+                    {this.displayTab()}
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+            </MediaQuery>
         </div>
       )
     }
--- app/components/template/StaticPage4.jsx
+++ app/components/template/StaticPage4.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import MediaQuery from 'react-responsive';
 export default class StaticPage3 extends React.Component {
   constructor(props) {
     super(props);
@@ -8,6 +8,7 @@ export default class StaticPage3 extends React.Component {
   render() {
     return (
       <div className="static-page4">
+            <MediaQuery query='(min-device-width: 1224px)'>
         <div className="content">
           <div className="bg">
             <div className="intro white">
@@ -39,6 +40,73 @@ export default class StaticPage3 extends React.Component {
             </div>
           </div>
         </div>
+                 </MediaQuery>
+        <MediaQuery query='(min-device-width: 768px) and (max-device-width: 1024px)'>
+            <div className="content">
+          <div className="bg">
+            <div className="intro white">
+              <div className="flex-container">
+                <div className="left-side">
+                  <div className="intro_heading">
+                    <h1> The National Examination Program "NEP" </h1>
+                  </div>
+                <div className="container">
+                  <div className="box1">
+                    <img src="/app/assets/images/template/icon_1.png"/>
+                    <div className="img_caption">Risk Based Approach</div>
+                  </div>
+                  <div className="box2">
+                    <img src="/app/assets/images/template/icon_2.png"/>
+                    <div className="img_caption">Emphasis on Team work</div>
+                  </div>
+                  <div className="box3">
+                    <img src="/app/assets/images/template/icon_3.png"/>
+                    <div className="img_caption">Enhance training of examiners</div>
+                  </div>
+                  <div className="box4">
+                    <img src="/app/assets/images/template/icon_4.png"/>
+                    <div className="img_caption">Hire senior industry professionals</div>
+                  </div>
+                </div>
+              </div>
+             </div>
+            </div>
+          </div>
+        </div>
+             </MediaQuery>
+        <MediaQuery query='(min-device-width: 320px) and (max-device-width: 736px)'>
+            <div className="content">
+          <div className="bg">
+            <div className="intro white">
+              <div className="flex-container">
+                <div className="left-side">
+                  <div className="intro_heading">
+                    <h1> The National Examination Program "NEP" </h1>
+                  </div>
+                <div className="container">
+                  <div className="box1">
+                    <img src="/app/assets/images/template/icon_1.png"/>
+                    <div className="img_caption">Risk Based Approach</div>
+                  </div>
+                  <div className="box2">
+                    <img src="/app/assets/images/template/icon_2.png"/>
+                    <div className="img_caption">Emphasis on Team work</div>
+                  </div>
+                  <div className="box3">
+                    <img src="/app/assets/images/template/icon_3.png"/>
+                    <div className="img_caption">Enhance training of examiners</div>
+                  </div>
+                  <div className="box4">
+                    <img src="/app/assets/images/template/icon_4.png"/>
+                    <div className="img_caption">Hire senior industry professionals</div>
+                  </div>
+                </div>
+              </div>
+             </div>
+            </div>
+          </div>
+        </div>
+             </MediaQuery>
       </div>
     )
   }
