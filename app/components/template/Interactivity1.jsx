be78309bca20d7de65c49fcd54e175f3b57a2784
abhishek <abhishek>
2017-05-11 17:44:02 +0530

    static page 2 and 3 for mobile devices

--- app/components/template/Interactivity1.jsx
+++ app/components/template/Interactivity1.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 
 export default class Interactivity1 extends React.Component {
   constructor(props) {
@@ -47,6 +48,7 @@ export default class Interactivity1 extends React.Component {
   render() {
     return (
       <div className="interactivity1">
+     <MediaQuery query='(min-device-width: 1224px)'>
         <div className="content">
           <div className="bg">
             <div className="intro white">
@@ -70,9 +72,9 @@ export default class Interactivity1 extends React.Component {
                     </ul>
                   </div>
                   <div className="btn_div">
-                    <span className="instruction_text">
+                    <span className="instruction_img">
                       <img src="/app/assets/images/template/hand_icon.png" className ="middle_align"/>
-                      <span>Click each button to know more.</span>
+                     <span className="instruction_text">Click each button to know more.</span>
                     </span>
                     <div className="buttons">
                       <span><button id="btn_01" onClick={this.openPopup.bind(this, 1)}>Approach 1</button></span>
@@ -89,6 +91,97 @@ export default class Interactivity1 extends React.Component {
             </div>
           </div>
         </div>
+          </MediaQuery>
+            
+              <MediaQuery query='(min-device-width: 768px) and (max-device-width: 1024px)'>
+        <div className="content">
+          <div className="bg">
+            <div className="intro white">
+              <div className="flex-container">
+                <div className="left-side">
+                  <div className="intro_heading">
+                    <h1>The National Examination Programe "NEP" </h1>
+                  </div>
+                  <div className="intro_para">
+                    <span>Senior Specialized Examiners with experience in:</span>
+                  </div>
+                  <div className="intro_list">
+                    <ul>
+                      <li>Trading</li>
+                      <li>Portfolio Management</li>
+                      <li>Valuation</li>
+                      <li>Complex Products</li>
+                      <li>Sales</li>
+                      <li>Compliance</li>
+                      <li>Forensic Accounting</li>
+                    </ul>
+                  </div>
+                  <div className="btn_div">
+                    <span className="instruction_img">
+                      <img src="/app/assets/images/template/hand_icon.png" className ="middle_align"/>
+                     <span className="instruction_text">Click each button to know more.</span>
+                    </span>
+                    <div className="buttons">
+                      <span><button id="btn_01" onClick={this.openPopup.bind(this, 1)}>Approach 1</button></span>
+                      <span><button id="btn_02" onClick={this.openPopup.bind(this, 2)}>Approach 2</button></span>
+                    </div>
+                  </div>
+                </div>
+                <div className="right-side">
+                  {
+                    this.displayPopup()
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+          </MediaQuery>
+            
+              <MediaQuery query='(min-device-width: 320px) and (max-device-width: 736px)'>
+        <div className="content">
+          <div className="bg">
+            <div className="intro white">
+              <div className="flex-container">
+                <div className="left-side">
+                  <div className="intro_heading">
+                    <h1>The National Examination Programe "NEP" </h1>
+                  </div>
+                  <div className="intro_para">
+                    <span>Senior Specialized Examiners with experience in:</span>
+                  </div>
+                  <div className="intro_list">
+                    <ul>
+                      <li>Trading</li>
+                      <li>Portfolio Management</li>
+                      <li>Valuation</li>
+                      <li>Complex Products</li>
+                      <li>Sales</li>
+                      <li>Compliance</li>
+                      <li>Forensic Accounting</li>
+                    </ul>
+                  </div>
+                  <div className="btn_div">
+                    <span className="instruction_img">
+                      <img src="/app/assets/images/template/hand_icon.png" className ="middle_align"/>
+                      <span className="instruction_text">Click each button to know more.</span>
+                    </span>
+                    <div className="buttons">
+                      <div><button id="btn_01" onClick={this.openPopup.bind(this, 1)}>Approach 1</button></div>
+                      <div><button id="btn_02" onClick={this.openPopup.bind(this, 2)}>Approach 2</button></div>
+                    </div>
+                  </div>
+                </div>
+                <div className="right-side">
+                  {
+                    this.displayPopup()
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+          </MediaQuery>
       </div>
     )
   }
--- app/components/template/StaticPage2.jsx
+++ app/components/template/StaticPage2.jsx
@@ -29,9 +29,9 @@ export default class StaticPage2 extends React.Component {
       							</ul>
       						</div>
       						<div className="btn">
-      							<span className="instruction_text">
+      							<span className="instruction_img">
                       <img src="/app/assets/images/template/hand_icon.png" className ="middle_align"/>
-                      <span>Click the button to know more.</span>
+                      <span className="instruction_text">Click the button to know more.</span>
                     </span>
                     <span>
                       <button id="btn_01">Benefits of this Course</button>
